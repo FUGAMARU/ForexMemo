f0657a906aa282e24f84e49cd40573cc5bddac21
FUGAMARU <gofugashi@gmail.com>
2021-12-20 23:40:01 +0900

    [Add] 通貨ペアリストのハイライト表示を切り替えできるように

--- components/SymbolCard.vue
+++ components/SymbolCard.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="flex justify-between px-5 py-2 my-2" :class="{'bg-white': isSelected, 'shadow-md': isSelected, 'bg-gray-200': !(isSelected)}" @click="highlight">
+	<div class="flex justify-between px-5 py-2 my-2">
 		<div class="flex">
 			<SymbolIcon :symbol="symbol"/>
 			<div class="text-2xl font-sb text-primary">{{symbol}}</div>
@@ -16,21 +16,7 @@ export default Vue.extend({
 		SymbolIcon: () => import("~/components/SymbolIcon.vue"),
 		Tag: () => import("~/components/Tag.vue")
 	},
-	props: ["symbol"],
-	data() {
-		return {
-			isSelected: false
-		}
-	},
-	methods: {
-		highlight() {
-			//alert(`Clicked symbol: ${this.symbol}`);
-			this.isSelected = !(this.isSelected)
-		},
-		/*resetHighlight() {
-			this.isSelected = false;
-		}*/
-	}
+	props: ["symbol"]
 })
 </script>
 
--- components/SymbolList.vue
+++ components/SymbolList.vue
@@ -1,7 +1,6 @@
 <template>
-	<!--<div class="grid-cols-1 bg-gray-200 py-3" @click="resetHighlights">-->
-	<div class="grid-cols-1 bg-gray-200 py-3">	
-		<SymbolCard v-for="(val, key) in symbols" :key="key" :symbol="val" ref="symbol"/>
+	<div class="grid-cols-1 bg-gray-200 py-3">
+		<SymbolCard v-for="(val, index) in symbols" :key="index" :symbol="val" @click.native="setHighlight(index)" :class="{'bg-white': currentCurrency === index, 'shadow-md': currentCurrency === index}" />
 	</div>
 </template>
 
@@ -14,14 +13,29 @@ export default Vue.extend({
 	},
 	data() {
 		return {
-			symbols: ["USD/JPY", "EUR/USD", "GBP/USD", "AUD/USD", "NZD/USD", "USD/CAD", "USD/CHF", "EUR/JPY", "GBP/JPY", "AUD/JPY", "NZD/JPY", "CAD/JPY", "CHF/JPY", "CAD/CHF", "EUR/CAD", "EUR/CHF", "EUR/GBP", "GBP/CAD", "GBP/CHF", "AUD/CAD", "AUD/CHF", "AUD/NZD", "EUR/AUD", "EUR/NZD", "GBP/AUD", "GBP/NZD", "NZD/CAD", "NZD/CHF"]
+			symbols: ["USD/JPY", "EUR/USD", "GBP/USD", "AUD/USD", "NZD/USD", "USD/CAD", "USD/CHF", "EUR/JPY", "GBP/JPY", "AUD/JPY", "NZD/JPY", "CAD/JPY", "CHF/JPY", "CAD/CHF", "EUR/CAD", "EUR/CHF", "EUR/GBP", "GBP/CAD", "GBP/CHF", "AUD/CAD", "AUD/CHF", "AUD/NZD", "EUR/AUD", "EUR/NZD", "GBP/AUD", "GBP/NZD", "NZD/CAD", "NZD/CHF"],
+			currentCurrency: <number | null>null,
+			prevCurrency: <string>""
 		}
 	},
 	methods: {
-		/*resetHighlights() {
-			//@ts-ignore
-			this.$refs.symbol.resetHighlight()
-		}*/
+		setHighlight(index: number) {
+			console.log(`Clicked: ${this.symbols[index]}`)
+			if(this.symbols[index] !== this.prevCurrency){
+				//チャート・メモを表示や切り替えしたい時
+				console.log(`Opened: ${this.symbols[index]}`)
+				this.prevCurrency = this.symbols[index]
+			}else{
+				//チャート・メモの表示を消したい時
+				console.log(`Closed: ${this.symbols[index]}`)
+			}
+
+			if(this.currentCurrency === index){
+				this.currentCurrency = null
+			}else{
+				this.currentCurrency = index
+			}
+		}
 	}
 })
 </script>
